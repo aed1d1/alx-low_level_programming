e6d62b3089af2b5a15c2b2d60c3ae9dec5fde6c0
Abdulrafiu Babatunde Edidi <125766005+aed1d1@users.noreply.github.com>
2023-05-31 11:14:17 +0100

    100-atoi Corrections

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,28 +9,32 @@
 int _atoi(char *s)
 {
 	int a = 0;
-	unsigned int ab = 0;
-	int mn = 1;
-	int mx = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+	int i = 0;
 
-	while (s[a])
+	while (s[c] != '\0')
+		c++;
+
+	while (i < c && d == 0)
 	{
-		if (s[a] == 45)
+		if (s[i] == '-')
+			++a;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			mn *= -1;
-		}
-		while (s[a] > 48 && s[a] <= 57)
-		{
-			mx = 1;
-			ab = (ab * 10) + (s[a] - '0');
-			a++;
-		}
-		if (mx == 1)
-		{
-			break;
-		}
-		a++;
-	}
-		ab *= mn;
-		return (ab);
+			e = s[i] - '0';
+			if (a % 2)
+				e = -e;
+			b = (b * 10 + e);
+			d = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			d = 0;
+		}    i++;
+	}    if (d == 0)
+		return (0);
+	return (b);
 }
