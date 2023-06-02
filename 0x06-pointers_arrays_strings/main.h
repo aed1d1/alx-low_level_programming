2a4f1e1f95feaab7ff41086528cfb545a74b705f
Abdulrafiu Babatunde Edidi <125766005+aed1d1@users.noreply.github.com>
2023-06-02 22:59:18 +0100

    main.h adjustment

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@ char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 int _strcmp(char *s1, char *s2);
-int _putchar(char n);
+int _putchar(char C);
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
