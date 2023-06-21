ebbaf875582b74e9793218c1ef74e699f0001781
Ajadi Edidi <edidi_abdulrafiu@yahoo.com>
2023-06-21 06:46:43 -0700

    Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index -  searches for an integer
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (a = 0; a < size; a++)
+	{
+		if (cmp(array[a]))
+			return (a);
+	}
+	return (-1);
+}
