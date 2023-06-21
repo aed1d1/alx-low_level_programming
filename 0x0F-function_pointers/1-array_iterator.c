7301f8e714a1bb5325cfaa80dc186c0c8dfb516b
Abdulrafiu Babatunde Edidi <125766005+aed1d1@users.noreply.github.com>
2023-06-21 14:40:09 +0100

    Task 1 Correction

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,13 +11,11 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int a;
+	size_t a;
 
 	if (array != NULL && action != NULL)
-		return;
-
-	for (a = 0; size > 0 && a < size; a++)
 	{
-		action(array[a]);
-	}
-}
+		for (a = 0; size > 0 && a < size; a++)
+		{
+			action(array[a]);
+		}}}
